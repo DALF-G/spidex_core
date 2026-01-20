4b866b06005e9cc216895ee2a78f0c4d6db61a69
dalf-g <muriithi.andrie@gmail.com>
2026-01-20 11:18:07 +0300

    new

--- config/db.js
+++ config/db.js
@@ -4,6 +4,9 @@ const { PrismaPg } = require("@prisma/adapter-pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
 const adapter = new PrismaPg(pool);
